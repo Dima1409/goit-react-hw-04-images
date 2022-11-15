fa521e0e9bdd0ffa96d37e0ba3902e2bd898655c
Dima <57713733+Dima1409@users.noreply.github.com>
2022-11-15 09:45:27 +0200

    edited useEffect

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -6,18 +6,17 @@ import PropTypes from 'prop-types';
 const modalRoot = document.querySelector('#modal-root');
 
 function ModalC({ original, desc, onClick }) {
-  useEffect(() => {
-    window.addEventListener('keydown', handleKey);
-    return () => {
-      window.removeEventListener('keydown', handleKey);
-    };
-  });
-
-  const handleKey = e => {
+  useEffect(() => {  
+    const handleKey = e => {
     if (e.code === 'Escape') {
       onClick();
     }
   };
+    window.addEventListener('keydown', handleKey);
+    return () => {
+      window.removeEventListener('keydown', handleKey);
+    };
+  }, [onClick]);
 
   const handleBackdrop = e => {
     if (e.currentTarget === e.target) {
